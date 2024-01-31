13b4e42f72d2ef54292e9e589a86bb1b5b932c12
Edon Gashi <egashi630@gmail.com>
2024-01-31 12:44:23 +0100

    almost finished section 6

--- Core/obj/Debug/net7.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net7.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d9cffaeda97d0447b52d8fdbf040551e82447b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2365d53587f057124b5110fc41a78a85d92bd92f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d9cffaeda97d0447b52d8fdbf040551e82447b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2365d53587f057124b5110fc41a78a85d92bd92f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
